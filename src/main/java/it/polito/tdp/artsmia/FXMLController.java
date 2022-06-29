a37f12bff5cf8db47201f9bbf51bd7f65b071998
s269152 <s269152@studenti.polito.it>
2022-06-29 09:45:10 +0200

    Versione Finale

--- src/main/java/it/polito/tdp/artsmia/FXMLController.java
+++ src/main/java/it/polito/tdp/artsmia/FXMLController.java
@@ -7,6 +7,7 @@ package it.polito.tdp.artsmia;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.artsmia.model.ArtObject;
 import it.polito.tdp.artsmia.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -47,12 +48,32 @@ public class FXMLController {
     
     @FXML
     void doAnalizzaOggetti(ActionEvent event) {
-
+    	this.model.creaGrafo();
+    	txtResult.appendText("\nGrafo Creato!"); 
+    	txtResult.appendText("\nVertici: "+this.model.nVertici()); 
+    	txtResult.appendText("\nArchi: "+this.model.nArchi()); 
+    	
     }
 
     @FXML
     void doCalcolaComponenteConnessa(ActionEvent event) {
-
+    	int objectId; 
+    	try {
+    		objectId=Integer.parseInt(txtObjectId.getText()); 
+    	}catch(NumberFormatException e ) {
+    		txtResult.appendText("Devi inserire un codice numerico!");
+    		return; 
+    	}
+    	
+    	ArtObject vertice = this.model.getObject(objectId);
+    	
+    	if(vertice == null) {
+    		txtResult.appendText("Oggetto inesistente!");
+    		return;
+    	}
+    	
+    	int size=this.model.getComponenteConnessa(vertice); 
+    	txtResult.appendText("\nDIMENSIONE COMPONENTE CONNESSA: " + size);
     }
 
     @FXML
